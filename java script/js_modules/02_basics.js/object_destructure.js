7cb22f27dc0d1e9baf8b19be07a63269523a122f
Shekh Azharuddin <133148447+skazhar786@users.noreply.github.com>
2025-02-09 21:01:00 +0530

    Delete java script directory

--- java script/js_modules/02_basics.js/object_destructure.js
+++
@@ -1,7 +0,0 @@
-const course = {
-    courseName: "js in hindi",
-    courseInstrustor:"shekhAzhar",
-    price:999
-}
-const {courseInstrustor:instructor} = course  // this is called object_destructure
-console.log(instructor);
